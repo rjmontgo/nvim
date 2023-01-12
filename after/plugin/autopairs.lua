a29bf32efafec6e58a2d5d6e7eea12cf9712d732
rjmontgo <robm4958@gmail.com>
2023-01-12 14:43:31 -0500

    Test

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -4,28 +4,7 @@ if not status_ok then
   return
 end
 
-npairs.setup {
-  check_ts = true,
-  ts_config = {
-    lua = { "string", "source" },
-    javascript = { "string", "template_string" },
-    java = false,
-    typescriptreact = true,
-  },
-  disable_filetype = { "TelescopePrompt" },
-  fast_wrap = {
-    map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'", },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0,
-    end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
-    check_comma = true,
-    highlight = 'Search',
-    highlight_grey = 'Comment',
-  },
-  enable_check_bracked_line = true
-}
+npairs.setup {}
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
