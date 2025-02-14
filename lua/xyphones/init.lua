5a098119384b6729d6ce90547ae578ea6f68b1c4
Rob Montgomery <rob.montgomery@usebutton.com>
2025-02-14 17:04:45 -0500

    clean

--- lua/xyphones/init.lua
+++
@@ -1,4 +0,0 @@
-require("xyphones.keymap")
-require("xyphones.options")
-require("xyphones.main")
-require("xyphones.autocmd")
--- lua/xyphones/keymap.lua
+++
@@ -1,8 +0,0 @@
-local opts = { silent = true, remap = true }
-
-local keymap = vim.keymap.set
-
-vim.g.mapleader = " "
-
-keymap("n", "]b", vim.cmd.bnext)
-keymap("n", "[b", vim.cmd.bprev)
